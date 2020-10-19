a458cfa95f81711745aaf0378376e5696095760e
Rafsan Zen M <73044188+RafsanZen@users.noreply.github.com>
2020-10-19 10:42:36 +0700

    Add files via upload

---
+++ PBOIF2-10119083-Latihan4-Kambing/src/pboif2/pkg10119083/latihan4/kambing/PBOIF210119083Latihan4Kambing.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119083.latihan4.kambing;
+
+/**
+ *
+ * @author 
+ * Nama : Rafsan Zen Mustaofa
+ * NIM  : 10119083
+ * Kelas: IF-2
+ * Deskripsi: Menamplkan Jumlah Kambing
+ */
+public class PBOIF210119083Latihan4Kambing {
+public void tambahKambing(){
+        int jumlahKambing = 0;
+
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah Kambing setelah ditambah: " + jumlahKambing);
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         PBOIF210119083Latihan4Kambing kambingJantan = new PBOIF210119083Latihan4Kambing();
+        kambingJantan.tambahKambing();
+    }
+    
+}
